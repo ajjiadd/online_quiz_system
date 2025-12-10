3cf6d6fdc2f0c9be178d883a363f0ad93d649e0a
ajjiad <ajjiad.cse@gmail.com>
2025-12-10 21:29:28 +0600

    added footer

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+    </main>
+    <footer class="footer">  <!-- Red footer like official sites -->
+        <div class="container">
+            <p>&copy; 2025 Online Quiz System. Developed for Learning. | Bangladesh Inspired Design</p>
+        </div>
+    </footer>
+    <script src="../assets/script.js"></script> 
+</body>
+</html>
